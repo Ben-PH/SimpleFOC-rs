1f739655a660be3339d52205a6f6cf1febadea08
Ben PHL <benphawke@gmail.com>
2024-05-02 16:26:03 +0200

    add position sensor stub

---
+++ src/pos_sensor.rs
@@ -0,0 +1,36 @@
+use core::marker::PhantomData;
+
+use embedded_hal::digital::InputPin;
+
+
+pub trait PosSensor: crate::types::MovementOrientation {
+    fn position(&self) -> ();
+    fn velocity(&self) -> ();
+    // fn acceleration(&self) -> ();
+
+    // for now, assuming an auto-magic update using pulse-counting hardware
+    // fn update_pos(&mut self);
+
+    fn is_homed(&self) -> bool;
+}
+
+pub struct ABEncoder<A: InputPin, B: InputPin> {
+    enc_a: A,
+    enc_b: B,
+    ppr: u16,
+    // Index pin
+    // todo: compile time configured presence of idx pin
+    
+    // counter setup
+    pulse_counter: u32,
+    pulse_timestamp: u64,
+
+    // velocity calc
+    prev_th: u32,
+    pulse_per_second: u32,
+    prev_ts_us: u64,
+}
+
+
+
+
