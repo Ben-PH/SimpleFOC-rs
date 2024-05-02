843a00150452ec9d7c655925a22a8b8dca3ad5a5
Ben PHL <benphawke@gmail.com>
2024-05-02 17:26:27 +0200

    refine and simplify foc_motor module

--- src/foc_motor.rs
+++ src/foc_motor.rs
@@ -1,11 +1,21 @@
 use crate::pos_sensor::PosSensor;
 
-pub enum MotionCtrlType {
-    Force,
-    Velocity,
-    Position,
-    VelocityOpenLoop,
-    PositionOpenLoop,
+/// The describes the position of an inductor in the pitch of the permenant magnetic field, in
+/// units of tau.
+/// A linear motor with a 20mm pitch, 10mm from reference zero, the value would be 0.5
+/// A rotary motor with a pitch of 36 degrees, and 9 inductors would have 12 degrees in
+/// the physical rotation of the motor for each phase-angle rotation.
+struct PhaseAngle(pub f32);
+struct Newtons(pub f32);
+struct MeterPerSecond(pub f32);
+struct Position(pub PhaseAngle);
+
+pub enum MotionCtrl {
+    Force(Newtons),
+    Velocity(MeterPerSecond),
+    Position(PhaseAngle),
+    VelocityOpenLoop(MeterPerSecond),
+    PositionOpenLoop(PhaseAngle),
 }
 pub enum ForceControlType {
     Voltage,
@@ -30,22 +40,14 @@ pub enum FOCModulationType {
     Trapezoid120,
     Trapezoid150,
 }
-pub struct FOCMotor<PS: PosSensor, CS> {
-    pos_sensor: PS,
-    current_sensor: CS,
-    velocity_limit: u32,
-    voltage_limit: u32,
-    current_limit: u32,
-    // foc_modulation: FOCModulationType,
-    // target
-}
 
-impl<PS: PosSensor, CS> FOCMotor<PS, CS> {
-    pub fn pos(&self) -> u32 {
-        todo!()
-    }
-    pub fn velocity(&self) -> i32 {
-        todo!()
-    }
+trait FOCMotor: Sized {
+    fn init() -> Result<Self, ()>;
+    fn enable(&mut self);
+    fn disable(&mut self);
+    // fn link_sensor/current_sensor(....
+    fn init_foc_algo(&mut self) -> u32; // why the u32?
+    fn foc_loop(&mut self) -> !;
+    fn move_command(motion: MotionCtrl);
+    fn set_phase_voltage(u_q: f32, u_d: f32, phase_angle: PhaseAngle);
 }
-
