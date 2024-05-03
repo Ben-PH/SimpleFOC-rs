cbefb2956cb55e5e17e1137f9ac26878fb36672d
Ben PHL <benphawke@gmail.com>
2024-05-03 10:35:34 +0200

    fleshout 6pwm impl

--- src/bldc_driver6pwm.rs
+++ src/bldc_driver6pwm.rs
@@ -1,9 +1,8 @@
-use embedded_hal::{
-    digital::{InputPin, OutputPin},
-    pwm::SetDutyCycle,
-};
+use embedded_hal::{digital::OutputPin, pwm::SetDutyCycle};
 
-use crate::{bldc_driver::PhaseState, foc_utils::PhaseVoltages};
+use crate::{
+    bldc_driver::{BLDCDriver, PhaseState}, foc_utils::PhaseVoltages,
+};
 
 pub struct BldcDriver6PWM<
     Ah: SetDutyCycle,
@@ -22,7 +21,49 @@ pub struct BldcDriver6PWM<
     cl_pin: Cl,
     enable: Option<En>,
 
-    phase_states: (PhaseState, PhaseState, PhaseState), 
+    phase_states: [PhaseState ; 3],
+    phase_voltages: PhaseVoltages,
+    duty_cycles: (f32, f32, f32),
+}
+
+impl<
+        Ah: SetDutyCycle,
+        Al: SetDutyCycle,
+        Bh: SetDutyCycle,
+        Bl: SetDutyCycle,
+        Ch: SetDutyCycle,
+        Cl: SetDutyCycle,
+        En: OutputPin,
+    > BLDCDriver for BldcDriver6PWM<Ah, Al, Bh, Bl, Ch, Cl, En>
+{
+    fn enable(&mut self) {
+        if let Some(mut enable_pin) = self.enable {
+            enable_pin.set_high();
+        }
+        self.set_phasestate([PhaseState::On; 3]);
+        self.set_pwm(Default::default());
+    }
+
+    fn disable(&mut self) {
+        self.set_phasestate([PhaseState::Off; 3]);
+        self.set_pwm(Default::default());
+        if let Some(mut enable_pin) = self.enable {
+            enable_pin.set_low();
+        }
+    }
+
+    fn set_pwm(&mut self, voltages: PhaseVoltages) {
+        // TODO: clamp between 0 and voltage limit
+
+
+        self.phase_voltages = PhaseVoltages { a: todo!(), b: todo!(), c: todo!() };
+        // TODO: write in `_writeDutyCycle6PWM`
+        todo!()
+    }
+
+    fn set_phasestate(&mut self, state: [PhaseState ; 3]) {
+        self.phase_states = state;
+    }
 }
 
 impl<
@@ -43,8 +84,10 @@ impl<
         ch_pin: Ch,
         cl_pin: Cl,
         enable: Option<En>,
-    ) -> Self {
-        Self {
+    ) -> Result<Self, ()> {
+        // TODO: account for voltage-limit config-vals
+        // TODO: hardware-specific configuration
+        Ok(Self {
             ah_pin,
             al_pin,
             bh_pin,
@@ -52,13 +95,13 @@ impl<
             ch_pin,
             cl_pin,
             enable,
+
+            duty_cycles: (0.0, 0.0, 0.0),
             phase_states: Default::default(),
-        }
+            phase_voltages: Default::default(),
+        })
     }
 
-    pub fn set_pwm(&mut self, voltages: PhaseVoltages) {
-        todo!()
-    }
     pub fn set_phase_states(states: (PhaseState, PhaseState, PhaseState)) {
         todo!()
     }
