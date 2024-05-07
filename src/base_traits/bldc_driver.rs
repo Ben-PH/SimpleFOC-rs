3b1bbdc02567444c5f7ebecc3dcfde12e71e4c32
Ben PHL <benphawke@gmail.com>
2024-05-07 15:49:33 +0200

    flesh out the bldc driver

--- src/base_traits/bldc_driver.rs
+++ src/base_traits/bldc_driver.rs
@@ -1,4 +1,6 @@
-use crate::common::helpers::PhaseVoltages;
+use embedded_hal::pwm::SetDutyCycle;
+
+use crate::common::helpers::{DutyCycle, PhaseVoltages};
 
 /// Describes what a given phase/coil/inductor is doing
 #[derive(Default, Copy, Clone)]
@@ -7,9 +9,72 @@ pub struct PhaseState {
     pub lo_side: bool,
 }
 
-pub trait BLDCDriver: Sized {
-    fn enable(&mut self);
-    fn disable(&mut self);
-    fn set_pwm(&mut self, voltages: PhaseVoltages);
+// for bldc: 3 and 6 pins.
+pub trait BLDCDriver: Sized + WriteDutyCycles {
+    // enable/disable pins are optional. will revisit this when I grok this
+    // fn enable(&mut self) -> Result<(), ()> {
+    //     self.set_pwm(
+    //         DutyCycle::try_new(0, 1.into().unwrap()),
+    //         DutyCycle::try_new(0, 1.into().unwrap()),
+    //         DutyCycle::try_new(0, 1.into().unwrap()),
+    //     )
+    // }
+
+    // fn disable(&mut self) -> Result<(), ()> {
+    //     self.set_pwm(
+    //         DutyCycle::try_new(0, 1.into().unwrap()),
+    //         DutyCycle::try_new(0, 1.into().unwrap()),
+    //         DutyCycle::try_new(0, 1.into().unwrap()),
+    //     )
+    // }
+    fn set_pwm(
+        &mut self,
+        voltages: PhaseVoltages,
+    ) -> Result<(), <Self as WriteDutyCycles>::SetError> {
+        todo!("constrain the phase voltages to be between 0 and 100%");
+        todo!("apply the duty cycles as a coeficient to the phase voltages");
+        let (a, b, c): (DutyCycle, DutyCycle, DutyCycle) =
+            todo!("constrain the duty cycles to between 0 and 100%");
+
+        <Self as WriteDutyCycles>::write_pwm_duty(self, a, b, c)
+    }
+
+    // In 3PWM, it's a bit weird. in PWM6, it's a simple `self.phasestate = state;`
     fn set_phasestate(&mut self, state: [PhaseState; 3]);
+}
+
+pub trait WriteDutyCycles {
+    type SetError;
+    fn write_pwm_duty(
+        &mut self,
+        duty_a: DutyCycle,
+        duty_b: DutyCycle,
+        duty_c: DutyCycle,
+    ) -> Result<(), Self::SetError>;
+}
+
+struct PWMPins<A: SetDutyCycle, B: SetDutyCycle, C: SetDutyCycle> {
+    pin_a: A,
+    pin_b: B,
+    pin_c: C,
+}
+
+impl<A: SetDutyCycle<Error = ()>, B: SetDutyCycle<Error = ()>, C: SetDutyCycle<Error = ()>>
+    WriteDutyCycles for PWMPins<A, B, C>
+{
+    type SetError = ();
+    fn write_pwm_duty(
+        &mut self,
+        duty_a: DutyCycle,
+        duty_b: DutyCycle,
+        duty_c: DutyCycle,
+    ) -> Result<(), Self::SetError> {
+        self.pin_a
+            .set_duty_cycle_fraction(duty_a.numer(), duty_a.denom().into())?;
+        self.pin_b
+            .set_duty_cycle_fraction(duty_b.numer(), duty_b.denom().into())?;
+        self.pin_c
+            .set_duty_cycle_fraction(duty_c.numer(), duty_c.denom().into())?;
+        Ok(())
+    }
 }
