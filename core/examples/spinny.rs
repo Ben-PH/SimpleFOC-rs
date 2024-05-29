93d04e6a5923fdd9cc7253f01af6000a41987cce
Ben PHL <benphawke@gmail.com>
2024-05-29 11:48:19 +0200

    comment the spinny e.g.

--- core/examples/spinny.rs
+++ core/examples/spinny.rs
@@ -14,14 +14,24 @@ fn foc_main(mut driver: SomePlatformSpecificImpl) -> !
 {
 
     loop {
+        // The "FO" part of the FOC. Here, we derive the position of the motor. We will use this to
+        // determine which electro-magnetic-field angle we wish to set.
         let angle = driver.get_position_um();
         let (sin, cos) = cordic::sin_cos(angle);
+
+        // we are setting 10% in the quadrature (torque) and 0% direct-axis
         let qd = park_clarke::MovingReferenceFrame{
             d: I16F16::ZERO,
             q: I16F16::from_num(0.1)
         };
+
+        // Orient our q/d values to the phase-angle we desire
         let inv_parke = foc::park_clarke::inverse_park(cos, sin, qd); 
+        // and use the ideal, though computationally heavy, scale-vector calculation for desired
+        // pwm duty-cycles, i.e. the percentage-of-maximum that each motor-phase will be set to
         let [dc_a, dc_b, dc_c] = foc::pwm::svpwm(inv_parke);
+
+        //... and set them.
         driver.set_pwms(DutyCycle(dc_a), DutyCycle(dc_b), DutyCycle(dc_c));
     }
 }
