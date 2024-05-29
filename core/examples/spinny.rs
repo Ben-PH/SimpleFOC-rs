f3c4c34ca9db852cb8a24c7ff2d35aaf890c54ec
Ben PHL <benphawke@gmail.com>
2024-05-29 13:57:45 +0200

    Refine spinny example

--- core/examples/spinny.rs
+++ core/examples/spinny.rs
@@ -1,6 +1,14 @@
-use fixed::{consts::FRAC_PI_3, types::I16F16};
+/// The sfoc equivilent of a micro-controller "blinky"
+///
+/// Running something modeled on this example, except with actual hardware, a motor that is at a
+/// fixed rpm, and outputting the pwm duty telemetry, you "should" see the same behavior.
+///
+/// Coming Soon™ will be actual code that you can run on $HARDWARE. It will use constant voltage,
+/// chosen such that a matching back-EMF is generated at around 30RPM.
+use std::time::SystemTime;
+
+use fixed::types::I16F16;
 use foc::park_clarke;
-/// The sfoc equivilent of "blinky"
 use sfoc_rs::{
     self,
     base_traits::{bldc_driver::MotorPins, foc_control::FOController, pos_sensor::PosSensor},
@@ -16,8 +24,11 @@ fn main() {
 // future, there will also be examples on how to write a portable implementation.
 fn foc_main(mut driver: SomePlatformSpecificImpl) -> ! {
     loop {
+
         // The "FO" part of the FOC. Here, we derive the position of the motor. We will use this to
         // determine which electro-magnetic-field angle we wish to set.
+        //
+        // In this example, we've hacked the sensor such that this method is hard-coded to rotate at 30rpm
         let angle = driver.get_position_um();
         let (sin, cos) = cordic::sin_cos(angle);
 
@@ -33,8 +44,12 @@ fn foc_main(mut driver: SomePlatformSpecificImpl) -> ! {
         // pwm duty-cycles, i.e. the percentage-of-maximum that each motor-phase will be set to
         let [dc_a, dc_b, dc_c] = foc::pwm::svpwm(inv_parke);
 
+
         //... and set them.
         driver.set_pwms(DutyCycle(dc_a), DutyCycle(dc_b), DutyCycle(dc_c));
+
+        // Let's artificially slow things down for the purpose of this example
+        std::thread::sleep(std::time::Duration::from_millis(117));
     }
 }
 
@@ -51,7 +66,7 @@ impl MotorPins for SomePlatformSpecificImpl {
         dc_b: sfoc_rs::common::helpers::DutyCycle,
         dc_c: sfoc_rs::common::helpers::DutyCycle,
     ) {
-        println!("DCs set: {}-{}-{}", dc_a.0, dc_b.0, dc_c.0);
+        println!("DCs set: {:>+6.4}|{:>+6.4}|{:>+6.4}", dc_a.0, dc_b.0, dc_c.0);
     }
 }
 
@@ -60,8 +75,11 @@ impl PosSensor for SomePlatformSpecificImpl {
 
     /// For an actual motor, this would read a position encoder, and give you a position. This
     /// might be milliradians, for example.
-    /// In this case, we are just doing pi/3, i.e. 60 degrees
+    /// In this case, we use std-time to get millis that rolls back to zero every 2 seconds
     fn get_position_um(&self) -> Self::Output {
-        I16F16::from_num(FRAC_PI_3)
+        let now = std::time::SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_millis();
+        let millis = now % 2000;
+        let frac = I16F16::from_num(millis) / 2000;
+        I16F16::TAU * frac
     }
 }
