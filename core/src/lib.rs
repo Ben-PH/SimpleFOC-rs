74a187914bcb123c62833da9d5972855bb868187
Ben PHL <benphawke@gmail.com>
2024-05-30 11:15:59 +0200

    Reorganise modules a touch

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,10 +1,12 @@
 #![no_std]
 
-pub mod base_traits;
-// pub mod bldc_driver6pwm;
+pub mod bldc_driver;
 pub mod bldc_motor;
 pub mod commands;
 pub mod common;
+pub mod current_sensor;
+pub mod foc_control;
 pub mod hw_drivers;
+pub mod pos_sensor;
 
 pub use pid as pid_reexported;
