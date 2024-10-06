12b39570abb17996a613b8b73110276c60a2fc21
Ben-PH <benphawke@gmail.com>
2024-10-06 18:53:40 +0200

    Add nix direnv support

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "A devShell example";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    esp32 = {
+      url = "github:knarkzel/esp32";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, esp32, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        idf-rust = esp32.packages.x86_64-linux.esp32;
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          buildInputs = [
+            openssl
+            pkg-config
+            eza
+            fd
+            idf-rust
+            rust-bin.stable.latest.default
+
+
+          ];
+
+          shellHook = ''
+            export PATH="${idf-rust}/.rustup/toolchains/esp/bin:$PATH"
+            export RUST_SRC_PATH="$(rustc --print sysroot)/lib/rustlib/src/rust/src"
+          '';
+        };
+      }
+    );
+}
+
