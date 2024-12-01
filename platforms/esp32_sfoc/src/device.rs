b5626335b23704b0b2e8de3aebe8cfdbe51217da
Ben-PH <benphawke@gmail.com>
2024-12-01 22:25:08 +0100

    bump esp-hal to 0.22

--- platforms/esp32_sfoc/src/device.rs
+++ platforms/esp32_sfoc/src/device.rs
@@ -3,12 +3,13 @@ use embedded_hal::pwm::SetDutyCycle;
 use esp_backtrace as _;
 use esp_hal::{
     clock::Clocks,
+    gpio::interconnect::PeripheralOutput,
     mcpwm::{
         operator::{PwmActions, PwmPin, PwmPinConfig, PwmUpdateMethod},
         timer::PwmWorkingMode,
-        PeripheralClockConfig, PwmPeripheral, MCPWM,
+        McPwm, PeripheralClockConfig, PwmPeripheral,
     },
-    pcnt::{channel, unit, PCNT},
+    pcnt::{channel, unit, Pcnt},
     peripheral::Peripheral,
     peripherals,
     prelude::*,
@@ -22,13 +23,10 @@ use sfoc_rs_core::{
 
 use crate::posn_encoder::EncoderPosn;
 
-pub struct Esp3PWM<'d, PwmOp, PinA, PinB, PinC, Pos> {
-    // mcpwm_periph: MCPWM<'d, PwmOp>,
-    motor_triplet: Triplet<
-        PwmPin<'d, PinA, PwmOp, 0, true>,
-        PwmPin<'d, PinB, PwmOp, 1, true>,
-        PwmPin<'d, PinC, PwmOp, 2, true>,
-    >,
+pub struct Esp3PWM<'d, PwmOp, Pos> {
+    // mcpwm_periph: McPwm<'d, PwmOp>,
+    motor_triplet:
+        Triplet<PwmPin<'d, PwmOp, 0, true>, PwmPin<'d, PwmOp, 1, true>, PwmPin<'d, PwmOp, 2, true>>,
     pulse_counter: PhantomData<Pos>,
     // time_src: PhantomData<Tim>,
     // _motion_ctrl: M,
@@ -37,37 +35,31 @@ pub struct Esp3PWM<'d, PwmOp, PinA, PinB, PinC, Pos> {
 impl<
         'd,
         PwmOp: PwmPeripheral,
-        PinA: esp_hal::gpio::OutputPin,
-        PinB: esp_hal::gpio::OutputPin,
-        PinC: esp_hal::gpio::OutputPin,
+        const UNIT_NUM: usize,
         // T: TimerGroupInstance,
     >
     Esp3PWM<
         'd,
         PwmOp,
-        PinA,
-        PinB,
-        PinC,
-        EncoderPosn,
+        EncoderPosn<'_, UNIT_NUM>,
         // TimerGroup<'d, T, Blocking>,
         // DefaultMotionCtrl<Timer0<T>, EncoderPosn>,
     >
 {
     /// Takes in the peripherals needed in order run a motor:
     ///  - pin_a/b/c: These pins will be attached to the mcpwm peripheral
-    ///  - enc_a/b: these pins will be attached to the PCNT peripheral.
+    ///  - enc_a/b: these pins will be attached to the Pcnt peripheral.
     ///  - esp32 has two timer groups and two mcpwm peripherals. you can pass in one of either
     ///  - the timer group will use one of its timers for the mcpwm operators.
     ///
     pub fn new(
-        clocks: &Clocks<'_>,
         // timg_choice: impl Peripheral<P = T> + 'd,
         mcpwm_choice: impl Peripheral<P = PwmOp> + 'd,
         pcnt_periph: impl Peripheral<P = peripherals::PCNT> + 'd,
         motor_pins: (
-            impl Peripheral<P = PinA> + 'd,
-            impl Peripheral<P = PinB> + 'd,
-            impl Peripheral<P = PinC> + 'd,
+            impl Peripheral<P = impl PeripheralOutput> + 'd,
+            impl Peripheral<P = impl PeripheralOutput> + 'd,
+            impl Peripheral<P = impl PeripheralOutput> + 'd,
         ),
         encoder_pins: (
             impl Peripheral<P = impl esp_hal::gpio::InputPin> + 'd,
@@ -77,24 +69,24 @@ impl<
         // set up the peripherals for our specific usecase
         // let timg0 = TimerGroup::new(timg_choice, clocks, None);
         // let time_src = Timer0::init(timg0.timer0);
-        let clock_cfg = PeripheralClockConfig::with_frequency(clocks, 40.MHz()).unwrap();
+        let clock_cfg = PeripheralClockConfig::with_frequency(40.MHz()).unwrap();
 
         // Boiler-plate configuration...
         let pin_config =
             || PwmPinConfig::<true>::new(PwmActions::empty(), PwmUpdateMethod::empty());
-        let mut mcpwm_periph = MCPWM::new(mcpwm_choice, clock_cfg);
+        let mut mcpwm_periph = McPwm::new(mcpwm_choice, clock_cfg);
         mcpwm_periph.operator0.set_timer(&mcpwm_periph.timer0);
         mcpwm_periph.operator1.set_timer(&mcpwm_periph.timer0);
         mcpwm_periph.operator2.set_timer(&mcpwm_periph.timer0);
 
         // Give each operator a pin.
-        let pin_a: PwmPin<'d, PinA, _, 0, true> = mcpwm_periph
+        let pin_a: PwmPin<'d, _, 0, true> = mcpwm_periph
             .operator0
             .with_pin_a(motor_pins.0, pin_config());
-        let pin_b: PwmPin<'d, PinB, _, 1, true> = mcpwm_periph
+        let pin_b: PwmPin<'d, _, 1, true> = mcpwm_periph
             .operator1
             .with_pin_a(motor_pins.1, pin_config());
-        let pin_c: PwmPin<'d, PinC, _, 2, true> = mcpwm_periph
+        let pin_c: PwmPin<'d, _, 2, true> = mcpwm_periph
             .operator2
             .with_pin_a(motor_pins.2, pin_config());
         // Put that into a Triplet. Because the pins meets the impl-constraints for
@@ -115,29 +107,15 @@ impl<
         // Let's get the party started.
         mcpwm_periph.timer0.start(pw_timer_cfg);
 
-        let pcnt = PCNT::new(pcnt_periph, None);
-        let mut pcnt_unit0 = pcnt.get_unit(unit::Number::Unit0);
-        pcnt_unit0
-            .configure(unit::Config {
-                low_limit: -100,
-                high_limit: 100,
-                ..Default::default()
-            })
-            .unwrap();
+        let pcnt = Pcnt::new(pcnt_periph);
+        let mut pcnt_unit0 = pcnt.unit0;
+        pcnt_unit0.set_low_limit(Some(-100));
+        pcnt_unit0.set_high_limit(Some(100));
 
-        let mut pcnt_chann0 = pcnt_unit0.get_channel(channel::Number::Channel0);
-        pcnt_chann0.configure(
-            channel::PcntSource::from_pin(encoder_pins.0),
-            channel::PcntSource::from_pin(encoder_pins.1),
-            channel::Config {
-                lctrl_mode: channel::CtrlMode::Reverse,
-                hctrl_mode: channel::CtrlMode::Keep,
-                pos_edge: channel::EdgeMode::Decrement,
-                neg_edge: channel::EdgeMode::Increment,
-                invert_ctrl: false,
-                invert_sig: false,
-            },
-        );
+        let mut pcnt_chann0 = &pcnt_unit0.channel0;
+        pcnt_chann0.set_ctrl_signal(encoder_pins.0);
+        pcnt_chann0.set_edge_signal(encoder_pins.1);
+        pcnt_chann0.set_input_mode(channel::EdgeMode::Decrement, channel::EdgeMode::Increment);
 
         Self {
             motor_triplet,
@@ -146,11 +124,11 @@ impl<
     }
 }
 
-impl<'d, PwmOp, A, B, C, Pos> MotorPins for Esp3PWM<'d, PwmOp, A, B, C, Pos>
+impl<'d, PwmOp, Pos> MotorPins for Esp3PWM<'d, PwmOp, Pos>
 where
-    PwmPin<'d, A, PwmOp, 0, true>: SetDutyCycle,
-    PwmPin<'d, B, PwmOp, 1, true>: SetDutyCycle,
-    PwmPin<'d, C, PwmOp, 2, true>: SetDutyCycle,
+    PwmPin<'d, PwmOp, 0, true>: SetDutyCycle,
+    PwmPin<'d, PwmOp, 1, true>: SetDutyCycle,
+    PwmPin<'d, PwmOp, 2, true>: SetDutyCycle,
 {
     fn set_pwms(&mut self, dc_a: DutyCycle, dc_b: DutyCycle, dc_c: DutyCycle) {
         self.motor_triplet.set_pwms(dc_a, dc_b, dc_c)
@@ -161,11 +139,11 @@ where
     }
 }
 
-impl<'d, PwmOp, A, B, C, Pos> FOController for Esp3PWM<'d, PwmOp, A, B, C, Pos>
+impl<'d, PwmOp, Pos> FOController for Esp3PWM<'d, PwmOp, Pos>
 where
-    PwmPin<'d, A, PwmOp, 0, true>: SetDutyCycle,
-    PwmPin<'d, B, PwmOp, 1, true>: SetDutyCycle,
-    PwmPin<'d, C, PwmOp, 2, true>: SetDutyCycle,
+    PwmPin<'d, PwmOp, 0, true>: SetDutyCycle,
+    PwmPin<'d, PwmOp, 1, true>: SetDutyCycle,
+    PwmPin<'d, PwmOp, 2, true>: SetDutyCycle,
 {
     fn set_psu_millivolt(&self, mv: u16) {
         todo!()
