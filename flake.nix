0a9b00e44b496a714458aff267ce8e0c9206cab6
Alexandria P. <alxpettit@gmail.com>
2024-11-30 17:26:14 -0700

    zero warnings in nixd & format with alejandra mitigations for nix being a bad language ;P

--- flake.nix
+++ flake.nix
@@ -11,31 +11,36 @@
       url = "github:knarkzel/esp32";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    flake-utils.url  = "github:numtide/flake-utils";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, esp32, nixpkgs, rust-overlay, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
+  outputs = {
+    esp32,
+    nixpkgs,
+    rust-overlay,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
         idf-rust = esp32.packages.x86_64-linux.esp32;
-        overlays = [ (import rust-overlay) ];
+        overlays = [(import rust-overlay)];
         pkgs = import nixpkgs {
           inherit system overlays;
         };
-      in
-      with pkgs;
-      {
-        devShells.default = mkShell {
-          buildInputs = [
-            openssl
-            pkg-config
-            eza
-            fd
-            idf-rust
-            rust-bin.stable.latest.default
-
-
-          ];
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs =
+            (with pkgs; [
+              openssl
+              pkg-config
+              eza
+              fd
+              rust-bin.stable.latest.default
+            ])
+            ++ [
+              idf-rust
+            ];
 
           shellHook = ''
             export PATH="${idf-rust}/.rustup/toolchains/esp/bin:$PATH"
@@ -45,4 +50,3 @@
       }
     );
 }
-
